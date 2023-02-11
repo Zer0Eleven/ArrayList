59f874038a8accf9f11da12db76ab822dc4dcc30
Zer0Eleven <112736327+Zer0Eleven@users.noreply.github.com>
2023-02-11 15:19:00 -0500

    How to use the array list

---
+++ howToUse.lua
@@ -0,0 +1,38 @@
+                                          --[[ How to use]]--
+--[[
+
+1. Loading the array
+
+  First, add a variable and name it whatever you'd like. I recommend naming it "array".
+  After you add the variable, You need to define it. After you define it, your code should be like this:
+
+  local array = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zer0Eleven/ArrayList/main/main.lua"))()
+
+  
+2. Adding stuff
+  
+  There are 4 functions, array.Add, array.Remove, array.Color, and array.Font. First, use array.Add to add an item!
+
+  heres how to use array.Add:
+  
+  array.Add("Speed", "CFrame")
+
+  Second, use array.Remove to remove an item! (Make sure that you've used array.Add to add the item you want to remove
+
+  Example: array.Remove("Speed")
+  
+  Third, use array.Color to set the color of the array to whatever you want (RGB), You can also use array.Color to make your own rainbow array!
+
+  Example: array.Color(255, 255, 255)
+  
+  Fourth, use array.Font to set the font of the array! (Make sure its a font supported by Roblox)
+
+  Example: array.Font(Enum.Font.Ubuntu)
+
+]]--
+
+--[[ Credits to whoever made the original library that I edited on ]]--
+
+
+
+
